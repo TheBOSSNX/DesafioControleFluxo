c6695848efbeaddd821516ce71c57d97f83db62b
TheBOSSNX <alcarvalho3301@gmail.com>
2023-10-21 16:19:53 -0400

    DesafioControleFluxo

---
+++ desafiocontrolefluxo/src/main/java/desafio/contador/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package desafio.contador;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
